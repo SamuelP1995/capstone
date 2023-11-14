2aae5412e3a0e3b28ad25e4008b23828cad824bd
SamuelP1995 <samuel_poirrier@yahoo.com>
2023-11-14 14:04:00 -0600

    renamed logout to logoutController, built logout logic in controller

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -4,7 +4,8 @@ const Models = require("../models");
 
 
 const logout = (req, res) => {
-    
+    // Clear user token on the server side
+    res.json({ success: true, message: 'User logged out successfully' });
 }
 
 
