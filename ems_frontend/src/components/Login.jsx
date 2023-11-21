be101e972f30013b5a0f2cf3d3f558629f9dbb4e
SamuelP1995 <samuel_poirrier@yahoo.com>
2023-11-21 13:38:05 -0600

    Further styling of login page, not done with logo

--- ems_frontend/src/components/Login.jsx
+++ ems_frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState} from "react";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Box, Card, CardActions, CardContent, CardMedia } from "@mui/material";
 import axios from "axios";
 
 export default function Login({setToken}) {
@@ -38,30 +38,47 @@ export default function Login({setToken}) {
       }
     }
 
-  
-    return (
-      <div>
-        <h2>Login</h2>
-        <form>
+    const card = (
+      <Card sx={{ maxWidth: 600 }}>
+        <CardMedia
+        sx={{ height: 150, width: 300 }}
+        image="https://dkedecals.com/cdn/shop/products/Green_Star_of_life_Helmet_Decal_grande.jpg?v=1549135315"
+        title="Ambulance Logo"
+        />
+        <CardContent>
+          <h2>Login</h2>
           <TextField variant="outlined" label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <br></br>
           <br></br>
           <TextField variant="outlined" label= "Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          {/* <div>
-            <label>Email:</label>
-            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div> */}
-          {/* <div>
-            <label>Password:</label>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </div> */}
           <br></br>
-          <br></br>
-          <Button variant="contained" type="button" onClick={handleLogin}>Login</Button>
-          <Button variant="contained" type="button" onClick={handleLogin}>New User</Button>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" type="button" color="success" size="large" m="50px" onClick={handleLogin}>Login</Button>
+          <Button variant="contained" type="button" color="success" size="large" p="50px" onClick={handleLogin}>New User</Button>
+        </CardActions>
+      </Card>
+    );
 
-        </form>
-      </div>
+
+    return (
+      <Box sx={{ minWidth: 275, variant: 'outlined', color: 'success' }} >
+        <Card variant='outlined' color='success' >{ card }</Card>
+      </Box>
     );
 }
   
+
+
+
+{/* <form>
+          <TextField variant="outlined" label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <br></br>
+          <br></br>
+          <TextField variant="outlined" label= "Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <br></br>
+          <br></br>
+          <Button variant="contained" type="button" color="success" size="large" m="20px" onClick={handleLogin}>Login</Button>
+          <Button variant="contained" type="button" color="success" size="large" p="20px" onClick={handleLogin}>New User</Button>
+
+        </form> */}
